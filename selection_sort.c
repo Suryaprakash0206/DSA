b4da6bdd674e8e880e7246127cca3d265b9bf831
Suryaprakash0206 <garapatisurya07@gmail.com>
2025-02-14 21:15:39 +0530

    Add files via upload

---
+++ selection_sort.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void sort(int arr[],int n){
+    for(int i=0;i<n;i++){
+        int min=i;
+
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[i]){
+                min=j;
+            }
+
+        }
+        int t=arr[i];
+         arr[i]=arr[min];
+        arr[min]=t;
+    }
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    sort(arr,n);
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+}

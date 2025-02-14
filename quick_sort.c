cd1aa0897ec2baf5fab23bd619e81fed665ab916
Suryaprakash0206 <garapatisurya07@gmail.com>
2025-02-14 21:11:44 +0530

    Add files via upload

---
+++ quick_sort.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int sort(int arr[],int low,int high){
+    
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            int temp = arr[i];
+            arr[j] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    i++;
+
+        int temp = arr[high];
+        arr[high] = arr[i];
+        arr[i] = temp;
+    return i;
+    
+}
+void quick(int arr[],int low,int high){
+    if(low<high){
+        int pivot=sort(arr,low,high);
+        sort(arr,low,pivot-1);
+        sort(arr,pivot+1,high);
+    }
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    quick(arr,0,n-1);
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+}

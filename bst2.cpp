bbdf333e4ed077ee183ad5e358faac941791948c
Suryaprakash0206 <garapatisurya07@gmail.com>
2025-07-18 09:52:05 +0530

    Add files via upload

---
+++ bst2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "binarysearchtree.h"
+using namespace std;
+int main()
+{
+    BinarySearchTree BST;
+    BST.Insert(10);
+    BST.Insert(20);
+    TreeNode *root;
+    BinarySearchTree BST1;
+    BST.ADD(20, root);
+}
